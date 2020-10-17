e8d1e0acbd41fd3ca174b182ffc4c6fc29506608
jeo19 <terrakkk@outlook.com>
2020-10-17 11:37:51 +0800

    add:Add the id for person array

---
+++ graphql/db.js
@@ -0,0 +1,32 @@
+export const Person = [
+    {
+        id: 0,
+        name: 'King Kong',
+        age: 20,
+        gender: 'female',
+    },
+    {
+        id: 1,
+        name: 'tenet',
+        age: 15,
+        gender: 'male',
+    },
+    {
+        id: 2,
+        name: 'Jhon work',
+        age: 10,
+        gender: 'female',
+    },
+    {
+        id: 3,
+        name: 'Dulacular',
+        age: 20,
+        gender: 'male',
+    },
+    {
+        id: 4,
+        name: 'Big shark',
+        age: 17,
+        gender: 'female',
+    },
+];
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,30 +1,4 @@
-const Person = [
-    {
-        name: 'King Kong',
-        age: 20,
-        gender: 'female',
-    },
-    {
-        name: 'tenet',
-        age: 15,
-        gender: 'male',
-    },
-    {
-        name: 'Jhon work',
-        age: 10,
-        gender: 'female',
-    },
-    {
-        name: 'Dulacular',
-        age: 20,
-        gender: 'male',
-    },
-    {
-        name: 'Big shark',
-        age: 17,
-        gender: 'female',
-    },
-];
+import { Person } from './db';
 const resolvers = {
     Query: {
         people: () => Person,
