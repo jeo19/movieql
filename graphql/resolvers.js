3ee4562b98b386176c9255f3eff9715d2b53ea71
jeo19 <terrakkk@outlook.com>
2020-10-20 11:22:51 +0800

    add:Add the mutation for adding the movie

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,11 @@
-import { movies, getById, getMovies } from './db';
+import { addMovie, getById, getMovies } from './db';
 const resolvers = {
     Query: {
         movies: () => getMovies(),
         movie: (_, { id }) => getById(id),
+    },
+    Mutation: {
+        addMovie: (_, { name, score }) => addMovie(name, score),
     },
 };
 export default resolvers;
